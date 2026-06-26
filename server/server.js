615d249dd7ebb95b7c2980cad13227963b7cdb51
Vigoraj_bty <bvigoraj@gmail.com>
2026-06-26 21:24:01 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const analyzeRoute = require("./routes/analyze");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: process.env.CLIENT_URL || "http://localhost:5173"}));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Analyzer API is running"));
